e29aa1a96352f06507321c83574aeeb05d720574
shivani0416 <shivanikawade80@gmail.com>
2025-08-05 18:18:25 +0530

    fix: port binding for render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import os
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 print(popular_df.columns)
@@ -51,5 +52,10 @@ def recommend():
 
     return render_template('recommend.html',data=data)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+
 
